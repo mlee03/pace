60e64f1c9352acbe416e6b51a8f53802c11f9442
mlee03 <Mikyung.Lee@lscamd50-d.gfdl.noaa.gov>
2024-01-29 13:17:46 -0500

    add files

--- .jenkins/checksum.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-set -e
-
-echo $(md5sum $@ | md5sum | awk '{print $1;}')
--- .jenkins/test_driver.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-JENKINS_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-
-PACE_IMAGE="driver_image" make -C ${JENKINS_DIR}/.. build
-docker run --rm driver_image make -C /pace/driver test test_mpi
